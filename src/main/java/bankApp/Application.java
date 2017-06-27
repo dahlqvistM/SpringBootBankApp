92939f7f12fca2b03814a67391f24774957f37ed
Matilda Dahlqvist <matilda_dahlqvist_@hotmail.com>
2017-06-27 07:28:08 +0200

    working endpoints

--- src/main/java/bankApp/Application.java
+++ src/main/java/bankApp/Application.java
@@ -1,5 +1,6 @@
 package bankApp;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
+    	System.out.println("Accessed main and started");
         SpringApplication.run(Application.class, args);
     }
+  
 }
 
--- src/main/java/bankApp/CustomerController.java
+++ src/main/java/bankApp/CustomerController.java
@@ -1,16 +1,23 @@
 package bankApp;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 
-@Controller
+@RestController
 public class CustomerController {
 
+	 private static final String template = "Hello, %s!";
+	
 	@RequestMapping("/bank")
-	public @ResponseBody String welcome(){
+	public String welcome(){
+		System.out.println("Accesed Welcome Test");
 		return "Welcome to the Bank";
 	}
+	
+	@RequestMapping("/getcustomer")
+	public Customer customer(@RequestParam(value="username", defaultValue="World") String username) {
+        return new Customer(null, null, String.format(template, username), username, username, username);
+    }
 }
